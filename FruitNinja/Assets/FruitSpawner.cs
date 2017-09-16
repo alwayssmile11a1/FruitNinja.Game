37d0e81cec5f52d995d664b80053871137b48ad5
Trương Ngọc Sơn <alwayssmile11a1@outlook.com>
2017-09-16 20:43:11 +0700

    FinishBasicGame

---
+++ FruitNinja/Assets/FruitSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    public GameObject fruit;
+    public Transform[] spawnPoints;
+    public float minDelay = 0.1f;
+    public float maxDelay = 1f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnFruits());
+    }
+	
+    IEnumerator SpawnFruits()
+    {
+        while(true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+
+
+            //Spawn Fruit
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            GameObject spawnedFruit = Instantiate(fruit, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedFruit, 5f);
+            
+        }
+    }
+
+}
